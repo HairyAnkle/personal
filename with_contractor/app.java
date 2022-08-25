2bd01c89cc3f6884d59b1ded82fd4e63793dd669
Cristian P. Ogena <111488747+HairyAnkle@users.noreply.github.com>
2022-08-25 15:51:23 +0800

    Update app.java

--- with_contractor/app.java
+++ with_contractor/app.java
@@ -2,13 +2,18 @@ public class App {
     public static void main(String[] args) {
 
         Character PhantomAssasin = new Character(5, 8, 3, "Mortred");
+        PhantomAssasin.sayMyName();
 
         Character NerubianAssassin = new Character(4, 6, 6, "Anub'arak");
+        NerubianAssassin.sayMyName();
 
         Character Blademaster = new Character(5, 10, 4, "Yurnero");
+        Blademaster.sayMyName();
 
         Character LegionCommander = new Character(9, 6, 4, "Legion Commander");
+        LegionCommander.sayMyName();
 
         Character ShadowFiend = new Character(4, 8, 6, "Nevermore");
+        ShadowFiend.sayMyName();
     }
 }
