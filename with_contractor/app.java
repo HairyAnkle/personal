398eca027275bc4d5db8a1abc3892c891b22e4ca
Cristian P. Ogena <111488747+HairyAnkle@users.noreply.github.com>
2022-08-25 15:45:43 +0800

    Create app.java

---
+++ with_contractor/app.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+
+        Character PhantomAssasin = new Character(5, 8, 3, "Mortred");
+
+        Character NerubianAssassin = new Character(4, 6, 6, "Anub'arak");
+
+        Character Blademaster = new Character(5, 10, 4, "Yurnero");
+
+        Character LegionCommander = new Character(9, 6, 4, "Legion Commander");
+
+        Character ShadowFiend = new Character(4, 8, 6, "Nevermore");
+    }
+}
