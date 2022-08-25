1b5218429d37719ff8d48ae9436727b2713f94ea
Cristian P. Ogena <111488747+HairyAnkle@users.noreply.github.com>
2022-08-25 15:46:03 +0800

    Create character.java

---
+++ with_contractor/character.java
@@ -0,0 +1,19 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+
+    public Character(int str, int agi, int intel, String characterName) {
+        name = characterName;
+        strength = str;
+        agility = agi;
+        intelligence = intel;
+    
+        System.out.println("My name is " + name + "\n" + "My Strength is " + strength + "\n" + "My Agility is " + agility + "\n" + "My intelligence is " + intelligence + "\n");
+    }
+}
